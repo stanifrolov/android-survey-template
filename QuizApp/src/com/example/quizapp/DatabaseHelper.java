e798d26a0df10fbb0adccb02c568f1b8dc29c1f2
stanislavfrolov92 <mail.stanislav.frolov@gmail.com>
2016-10-23 10:10:06 +0200

    Refactor

--- QuizApp/src/com/example/quizapp/DatabaseHelper.java
+++ QuizApp/src/com/example/quizapp/DatabaseHelper.java
@@ -15,136 +15,102 @@ import java.util.List;
 public class DatabaseHelper extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "QuizApp";
-    private static final String TABLE_QUEST = "quest";
-    private static final String KEY_ID = "id";
-    private static final String KEY_QUESTION = "question";
-    private static final String KEY_CORRECT_ANSWER = "answer";
-    private static final String KEY_OPTION_A = "optionA";
-    private static final String KEY_OPTION_B = "optionB";
-    private static final String KEY_OPTION_C = "optionC";
-    private SQLiteDatabase dbase;
+    private static final String TABLE_QUESTION = "quiz_table";
+    private static final String KEY_ID = "ID";
+    private static final String KEY_QUESTION = "Question";
+    private static final String KEY_CORRECT_ANSWER = "CorrectAnswer";
+    private static final String KEY_OPTION_A = "OptionA";
+    private static final String KEY_OPTION_B = "OptionB";
+    private static final String KEY_OPTION_C = "OptionC";
+    private SQLiteDatabase database;
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase db) {
-        dbase = db;
-        String sql = "CREATE TABLE IF NOT EXISTS " + TABLE_QUEST + " ( "
+    public void onCreate(SQLiteDatabase sqlitedatabase) {
+        database = sqlitedatabase;
+        String sql = "CREATE TABLE IF NOT EXISTS " + TABLE_QUESTION + " ( "
                 + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + KEY_QUESTION
                 + " TEXT, " + KEY_CORRECT_ANSWER + " TEXT, " + KEY_OPTION_A + " TEXT, "
                 + KEY_OPTION_B + " TEXT, " + KEY_OPTION_C + " TEXT)";
-        db.execSQL(sql);
+        sqlitedatabase.execSQL(sql);
         addQuestions();
         // TODO: 21.10.2016 Check if this is needed
-        //db.close();
+        // sqlitedatabase.close();
     }
 
     private void addQuestions() {
-        Question q1 = new Question("What is JP?", "Jalur Pesawat", "Jack sParrow", "Jasa Programmer", "Jasa Programmer");
-        this.addQuestion(q1);
-        Question q2 = new Question("where the JP place?", "Monas, Jakarta", "Gelondong, Bangun Tapan, bantul", "Gelondong, Bangun Tapan, bandul", "Gelondong, Bangun Tapan, bantul");
-        this.addQuestion(q2);
-        Question q3 = new Question("who is CEO of the JP?", "Usman and Jack", "Jack and Rully", "Rully and Usman", "Rully and Usman");
-        this.addQuestion(q3);
-        Question q4 = new Question("what do you know about JP?", "JP is programmer home", "JP also realigy home", "all answer is true", "all answer is true");
-        this.addQuestion(q4);
-        Question q5 = new Question("what do you learn in JP?", "Realigy", "Programming", "all answer is true", "all answer is true");
-        this.addQuestion(q5);
+        Question question = new Question("How do you feel?", "Good", "Okay", "Bad", "Any answer is correct");
+        this.addQuestion(question);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldV, int newV) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_QUEST);
-        onCreate(db);
+    public void onUpgrade(SQLiteDatabase sqlitedatabase, int oldV, int newV) {
+        sqlitedatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_QUESTION);
+        onCreate(sqlitedatabase);
     }
 
-    public void addQuestion(Question quest) {
-        //SQLiteDatabase db = this.getWritableDatabase();
+    public void addQuestion(Question question) {
         ContentValues values = new ContentValues();
-        values.put(KEY_QUESTION, quest.getQuestion());
-        values.put(KEY_CORRECT_ANSWER, quest.getAnswer());
-        values.put(KEY_OPTION_A, quest.getOptionA());
-        values.put(KEY_OPTION_B, quest.getOptionB());
-        values.put(KEY_OPTION_C, quest.getOptionC());
-        dbase.insert(TABLE_QUEST, null, values);
+        values.put(KEY_QUESTION, question.getQuestion());
+        values.put(KEY_CORRECT_ANSWER, question.getCorrectAnswer());
+        values.put(KEY_OPTION_A, question.getOptionA());
+        values.put(KEY_OPTION_B, question.getOptionB());
+        values.put(KEY_OPTION_C, question.getOptionC());
+        database.insert(TABLE_QUESTION, null, values);
     }
 
     public List<Question> getAllQuestions() {
-        List<Question> quesList = new ArrayList<Question>();
-        // Select All Query
-        String selectQuery = "SELECT  * FROM " + TABLE_QUEST;
-        dbase = this.getReadableDatabase();
-        Cursor cursor = dbase.rawQuery(selectQuery, null);
-        // looping through all rows and adding to list
+        List<Question> allQuestions = new ArrayList<Question>();
+        String selectQuery = "SELECT  * FROM " + TABLE_QUESTION;
+        database = this.getReadableDatabase();
+        Cursor cursor = database.rawQuery(selectQuery, null);
         if (cursor.moveToFirst()) {
             do {
-                Question quest = new Question();
-                quest.setId(cursor.getInt(0));
-                quest.setQuestion(cursor.getString(1));
-                quest.setAnswer(cursor.getString(2));
-                quest.setOptionA(cursor.getString(3));
-                quest.setOptionB(cursor.getString(4));
-                quest.setOptionC(cursor.getString(5));
-                quesList.add(quest);
+                Question question = new Question();
+                question.setId(cursor.getInt(0));
+                question.setQuestion(cursor.getString(1));
+                question.setCorrectAnswer(cursor.getString(2));
+                question.setOptionA(cursor.getString(3));
+                question.setOptionB(cursor.getString(4));
+                question.setOptionC(cursor.getString(5));
+                allQuestions.add(question);
             } while (cursor.moveToNext());
         }
-        return quesList;
-    }
-
-    public int rowcount() {
-        int row = 0;
-        String selectQuery = "SELECT  * FROM " + TABLE_QUEST;
-        SQLiteDatabase db = this.getWritableDatabase();
-        Cursor cursor = db.rawQuery(selectQuery, null);
-        row = cursor.getCount();
-        return row;
+        return allQuestions;
     }
 
     public ArrayList<Cursor> getData(String Query) {
-        //get writable dbase
-        SQLiteDatabase dbase = this.getWritableDatabase();
-        String[] columns = new String[]{"mesage"};
-        //an array list of cursor to save two cursors one has results from the query
-        //other cursor stores error message if any errors are triggered
-        ArrayList<Cursor> alc = new ArrayList<Cursor>(2);
-        MatrixCursor Cursor2 = new MatrixCursor(columns);
-        alc.add(null);
-        alc.add(null);
-
+        database = this.getReadableDatabase();
+        String[] columns = new String[]{"message"};
+        ArrayList<Cursor> allCursor = new ArrayList<Cursor>(2);
+        MatrixCursor matrixCursor = new MatrixCursor(columns);
+        allCursor.add(null);
+        allCursor.add(null);
 
         try {
             String maxQuery = Query;
-            //execute the query results will be save in Cursor c
-            Cursor c = dbase.rawQuery(maxQuery, null);
-
-            //add value to cursor2
-            Cursor2.addRow(new Object[]{"Success"});
-
-            alc.set(1, Cursor2);
+            Cursor c = database.rawQuery(maxQuery, null);
+            matrixCursor.addRow(new Object[]{"success"});
+            allCursor.set(1, matrixCursor);
             if (null != c && c.getCount() > 0) {
-
-                alc.set(0, c);
+                allCursor.set(0, c);
                 c.moveToFirst();
-
-                return alc;
+                return allCursor;
             }
-            return alc;
-        } catch (SQLiteException sqlEx) {
-            Log.d("printing exception", sqlEx.getMessage());
-            //if any exceptions are triggered save the error message to cursor an return the arraylist
-            Cursor2.addRow(new Object[]{"" + sqlEx.getMessage()});
-            alc.set(1, Cursor2);
-            return alc;
+            return allCursor;
+        } catch (SQLiteException sqliteEx) {
+            Log.d("printing exception", sqliteEx.getMessage());
+            matrixCursor.addRow(new Object[]{"" + sqliteEx.getMessage()});
+            allCursor.set(1, matrixCursor);
+            return allCursor;
         } catch (Exception ex) {
-
             Log.d("printing exception", ex.getMessage());
-
-            //if any exceptions are triggered save the error message to cursor an return the arraylist
-            Cursor2.addRow(new Object[]{"" + ex.getMessage()});
-            alc.set(1, Cursor2);
-            return alc;
+            matrixCursor.addRow(new Object[]{"" + ex.getMessage()});
+            allCursor.set(1, matrixCursor);
+            return allCursor;
         }
 
     }
